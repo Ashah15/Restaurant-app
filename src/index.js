9ab72b3a433be6259aa67574a7e014d08d3e0555
Ashah15 <ashaabdillahi53@gmail.com>
2020-11-14 00:03:57 +0300

    add the build and start scripts in the package.json file

--- src/index.js
+++ src/index.js
@@ -4,32 +4,7 @@ import Menu from './menu';
 import spaghettiImg from './spaghetti.jpg';
 import foodImg from './food.jpg';
 import beansImg from './beans.jpg';
-
-const initialLoad = () => {
-  const headerInfo = document.createElement('div');
-  headerInfo.classList.add('row', 'header__info');
-
-  const headerTextContent = document.createElement('div');
-  headerTextContent.classList.add('col', 'header__text-content');
-
-  const headerTitle = document.createElement('h2');
-  headerTitle.classList.add('header__title');
-  headerTitle.textContent = 'Ashahs Eatery';
-
-  const headerContent = document.createElement('p');
-  headerContent.classList.add('header__content');
-  headerContent.textContent = 'Satisfying people’s hunger for life’s simple pleasures';
-
-  headerTextContent.appendChild(headerTitle);
-  headerTextContent.appendChild(headerContent);
-
-  const headerImgContent = document.createElement('div');
-  headerImgContent.classList.add('col', 'header__img-content');
-
-  headerInfo.appendChild(headerTextContent);
-
-  return headerInfo;
-};
+import initialLoad from './load';
 
 const setBackground = (image) => {
   document.body.style.backgroundImage = `url(${image})`;
